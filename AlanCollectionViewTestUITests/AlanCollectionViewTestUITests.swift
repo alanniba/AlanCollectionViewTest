e17b0f7bf59ee733f9155ad3abc256aa893e55e8
haoyuan tan <957171869@qq.com>
2018-10-04 11:57:18 -0400

    Initial Commit

---
+++ AlanCollectionViewTestUITests/AlanCollectionViewTestUITests.swift
@@ -0,0 +1,36 @@
+//
+//  AlanCollectionViewTestUITests.swift
+//  AlanCollectionViewTestUITests
+//
+//  Created by haoyuan tan on 2018/10/4.
+//  Copyright © 2018年 haoyuan tan. All rights reserved.
+//
+
+import XCTest
+
+class AlanCollectionViewTestUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
